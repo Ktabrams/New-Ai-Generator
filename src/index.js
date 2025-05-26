ef25c7d103a5a79107d0a9e90f7c894924bd5694
Ktabrams <kabrams183@aol.com>
2025-05-25 20:47:15 -0400

    reworded call to make ai more accurate

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "678ata03f1eobe6565f0009f45ba1d99";
   let prompt = `Generate a poem about ${instructionsInput.value}`;
-  let context ="You are a romantic poem expert. You want to write a poem that is short and beautiful. Your mission is to generate a 4-line poem and seperate each line with a <br />. Make sure to follow the user instructions. And sign the end of the poem with 'SheCodes AI' in a <strong> element";
+  let context ="You are a romantic poem expert. Your mission is to generate a beautiful 4-line poem and seperate each line with a <br />. Make sure to follow the user instructions. And sign the end of the poem with 'SheCodes AI' in a <strong> element";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=~${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
